104a3ed3eaa6ed840ad55f4c7d41fb3a08b08f93
Thien Trieu <thien.trieu@telus.com>
2023-02-01 17:25:16 -0800

    Adding user seeds

--- server/src/db/schema/create.sql
+++ server/src/db/schema/create.sql
@@ -5,8 +5,8 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  profile_picture VARCHAR(255) DEFAULT 'https://github.com/thien-trieu/foodie-connect/blob/master/server/docs/default_avatar.png?raw=true',
-  description VARCHAR(100),
-  location VARCHAR(255),
+  location VARCHAR(255) NOT NULL,
+  avatar VARCHAR(255) DEFAULT 'https://github.com/thien-trieu/foodie-connect/blob/master/server/docs/default_avatar.png?raw=true',
+  bio VARCHAR(100) DEFAULT '',
   created_at timestamp default current_timestamp
 );
