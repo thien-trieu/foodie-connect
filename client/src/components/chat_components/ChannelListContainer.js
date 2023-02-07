3638dc6c74c1dbd601ce1f87176d3ac69b7a4fe2
Thien Trieu <thien.trieu@telus.com>
2023-02-06 20:34:53 -0800

    Update logo on chat

--- client/src/components/chat_components/ChannelListContainer.js
+++ client/src/components/chat_components/ChannelListContainer.js
@@ -3,8 +3,10 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
-import HospitalIcon from '../../assets/hospital.png'
-import LogoutIcon from '../../assets/logout.png'
+import HospitalIcon from '../../assets/3.png'
+import LogoutIcon from '../../assets/Logout.png'
+
+import logo from '../../assets/5.png';
 
 const cookies = new Cookies();
 
@@ -25,7 +27,8 @@ const SideBar = ({ logout }) => (
 
 const CompanyHeader = () => (
   <div className="channel-list__header">
-    <p className="channel-list__header__text">Medical Pager</p>
+    <img src={logo} alt="logo" className="chat-logo" />
+    
   </div>
 )
 
