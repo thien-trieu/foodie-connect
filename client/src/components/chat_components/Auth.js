f08b6cd4b3c3e7644887b57ac51b19e85f63e720
Carol Cheung <carol.cheung@telus.com>
2023-02-06 23:11:10 -0800

    css styles added to login and channel create

--- client/src/components/chat_components/Auth.js
+++ client/src/components/chat_components/Auth.js
@@ -145,8 +145,8 @@ function Auth() {
           <div className="auth__form-container_fields-account">
             <p>
               {isSignup
-                ? "Already have an account?"
-                : "Don't have an account?"
+                ? "Already have an account? "
+                : "Don't have an account? "
               }
               <span onClick={switchMode}>
                 {isSignup ? 'Sign In' : 'Sign Up'}
