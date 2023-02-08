c839f207695c74b5537da2f5c91ed3a64a81646f
Carol Cheung <carol.cheung@telus.com>
2023-02-07 22:24:23 -0800

    remove edit channel from public channels

--- client/src/components/chat_components/ChannelInner.js
+++ client/src/components/chat_components/ChannelInner.js
@@ -33,7 +33,7 @@ const ChannelInner = ({ setIsEditing }) => {
       setGiphyState(false);
     }
   };
-//giphyContext.provider retrieves giphys and gives the other child components and any descendants access
+  //giphyContext.provider retrieves giphys and gives the other child components and any descendants access
   return (
     <GiphyContext.Provider value={{ giphyState, setGiphyState }}>
       <div style={{ display: 'flex', width: '100%' }}>
@@ -52,52 +52,55 @@ const ChannelInner = ({ setIsEditing }) => {
 
 //display name and users online and avatars inside header
 const TeamChannelHeader = ({ setIsEditing }) => {
-    const { channel, watcher_count } = useChannelStateContext();
-    const { client } = useChatContext();
-  
-    const MessagingHeader = () => {
-      const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userID);
-      const additionalMembers = members.length - 3;
-  
-      if(channel.type === 'messaging') {
-        return (
-          <div className='team-channel-header__name-wrapper'>
-            {members.map(({ user }, i) => (
-              <div key={i} className='team-channel-header__name-multi'>
-                <Avatar image={user.image} name={user.fullName || user.id} size={32} />
-                <p className='team-channel-header__name user'>{user.fullName || user.id}</p>
-              </div>
-            ))}
-  
-            {additionalMembers > 0 && <p className='team-channel-header__name user'>and {additionalMembers} more</p>}
-          </div>
-        );
-      }
-  
+  const { channel, watcher_count } = useChannelStateContext();
+  const { client } = useChatContext();
+
+  const MessagingHeader = () => {
+
+    const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userID);
+    const additionalMembers = members.length - 3;
+
+    if (channel.type === 'messaging') {
       return (
-        <div className='team-channel-header__channel-wrapper'>
-          <p className='team-channel-header__name'># {channel.data.name}</p>
+        <div className='team-channel-header__name-wrapper'>
+          {members.map(({ user }, i) => (
+            <div key={i} className='team-channel-header__name-multi'>
+              <Avatar image={user.image} name={user.fullName || user.id} size={32} />
+              <p className='team-channel-header__name user'>{user.fullName || user.id}</p>
+            </div>
+          ))}
+
+          {additionalMembers > 0 && <p className='team-channel-header__name user'>and {additionalMembers} more</p>}
+        </div>
+      );
+    }
+
+    return (
+      <div className='team-channel-header__channel-wrapper'>
+        <p className='team-channel-header__name'># {channel.data.name}</p>
+        {channel.type !== 'public' && (
           <span style={{ display: 'flex' }} onClick={() => setIsEditing(true)}>
             <ChannelInfo />
           </span>
-        </div>
-      );
-    };
-  
-    const getWatcherText = (watchers) => {
-      if (!watchers) return 'No users online';
-      if (watchers === 1) return '1 user online';
-      return `${watchers} users online`;
-    };
-  
-    return (
-      <div className='team-channel-header__container'>
-        <MessagingHeader />
-        <div className='team-channel-header__right'>
-          <p className='team-channel-header__right-text'>{getWatcherText(watcher_count)}</p>
-        </div>
+        )}
       </div>
     );
   };
+
+  const getWatcherText = (watchers) => {
+    if (!watchers) return 'No users online';
+    if (watchers === 1) return '1 user online';
+    return `${watchers} users online`;
+  };
+
+  return (
+    <div className='team-channel-header__container'>
+      <MessagingHeader />
+      <div className='team-channel-header__right'>
+        <p className='team-channel-header__right-text'>{getWatcherText(watcher_count)}</p>
+      </div>
+    </div>
+  );
+};
 
 export default ChannelInner
